c6cec5b9b4395f64a620bffbf477986bde908696
mhsjaber <mhsjaber@gmail.com>
2017-07-13 21:50:38 +0600

    init

---
+++ AsyncFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult AsyncUpload(IEnumerable<HttpPostedFileBase> files)
+        {
+            int count = 0;
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/UploadFiles" + filename));
+                        file.SaveAs(path);
+                        count++;
+                    }
+                }
+            }
+            return new JsonResult { Data = "Jaber"};
+        }
+    }
+}
---
+++ AsyncFileUpload/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AsyncFileUpload.MvcApplication" Language="C#" %>
